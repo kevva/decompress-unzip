355f60e003e44f298e823e06e44798370bc66deb
Kevin Martensson <kevinmartensson@gmail.com>
2014-09-13 22:36:46 +0200

    Use vinyl-file for testing

--- test/test.js
+++ test/test.js
@@ -1,22 +1,18 @@
 'use strict';
 
-var File = require('vinyl');
-var fs = require('fs');
 var isJpg = require('is-jpg');
 var path = require('path');
+var read = require('vinyl-file').read;
 var test = require('ava');
 var zip = require('../');
 
 test('decompress a ZIP file', function (t) {
     t.plan(2);
 
-    fs.readFile(path.join(__dirname, 'fixtures/test.zip'), function (err, buf) {
+    read(path.join(__dirname, 'fixtures/test.zip'), function (err, file) {
         t.assert(!err);
 
         var stream = zip();
-        var file = new File({
-            contents: buf
-        });
 
         stream.on('data', function (file) {
             t.assert(isJpg(file.contents));
@@ -29,13 +25,10 @@ test('decompress a ZIP file', function (t) {
 test('strip path level using the `strip` option', function (t) {
     t.plan(3);
 
-    fs.readFile(path.join(__dirname, 'fixtures/test-nested.zip'), function (err, buf) {
+    read(path.join(__dirname, 'fixtures/test-nested.zip'), function (err, file) {
         t.assert(!err);
 
         var stream = zip({ strip: 1 });
-        var file = new File({
-            contents: buf
-        });
 
         stream.on('data', function (file) {
             t.assert(file.path === 'test.jpg');
